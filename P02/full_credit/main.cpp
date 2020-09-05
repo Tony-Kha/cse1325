8c12d163c7ea4ee88615f00468334a4bd24c36b5
Tony Kha <tony.kha@mavs.uta.edu>
2020-09-05 17:48:41 -0500

    rolling

--- P02/full_credit/main.cpp
+++ P02/full_credit/main.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
+
+void roll(int num, int sides, int rolls){
+    for(int i=0; i<rolls; i++){
+        std::vector<int>v;
+        for(int j=0; j<num; j++){
+            v.push_back(rand()%sides+1);
+        }
+        std::cout<<"Roll"<<i+1<<": ";
+        for(auto x : v) std::cout<<x<<" ";
+        std::cout<<"\n";
+    }
+}
 
 int main() {
+    srand(time(NULL));
+
     int num, sides, rolls;
     std::cout<<"Enter the number of dice to roll"<<std::endl;
     std::cin>>num;
 
     std::cout<<"Enter the number of sides for each dice"<<std::endl;
     std::cin>>sides;
-    std::vector<int>v;
-    for(int i=0; i<sides;i++) v.push_back(i+1);
 
     std::cout<<"Enter the number of times to roll"<<std::endl;
     std::cin>>rolls;
+
+    roll(num,sides,rolls);
 }
 
