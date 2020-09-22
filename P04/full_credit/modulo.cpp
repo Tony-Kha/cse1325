fb37d472b127e4c220f93032df8ec05b44a6af34
Tony Kha <tony.kha@mavs.uta.edu>
2020-09-21 21:56:05 -0500

    nmsd

--- P04/full_credit/modulo.cpp
+++ P04/full_credit/modulo.cpp
@@ -2,16 +2,15 @@
 #include <iostream>
 
 int Modulo::compare(const int rhs){
-    if(_value < rhs._value) return -1;
-    if(_value > rhs._value) return 1;
+    if(_value < rhs) return -1;
+    if(_value > rhs) return 1;
     return 0;
 }
 
 Modulo::Modulo(int modulo, int value, int offset):_modulo{modulo}, _value{value}, _offset{offset}{}
 
-Modulo Modulo::set_nmsd(Modulo* nmsd){ 
-    if(++_value==_offset)
-        ++nmsd._value;
+void Modulo::set_nmsd(Modulo* nmsd){ 
+    _nmsd = nmsd;
 }
 
 int Modulo::value(){return _value;}
@@ -28,14 +27,16 @@ Modulo Modulo::operator+(int rhs){
 }
 
 Modulo& Modulo::operator++(){
-    if(_value == _modulo) 
+    if(_value == _modulo){
         _value = _offset;
+        ++_nmsd->_value;
+    }
     else
         ++_value;
     return *this;
 }
 
-Modulo Modulo::operator++(int ignored){
+Modulo Modulo::operator++(int){
     Modulo modulo{*this};
     ++*this;
     return modulo;
@@ -48,7 +49,7 @@ std::ostream& operator<<(std::ostream& ost, Modulo& m){
 
 std::istream& operator>>(std::istream& is, Modulo& m){
     is>>m._value;
-    if(m._value > m.modulo)
+    if(m._value > m._modulo)
         throw std::runtime_error{"Invalid number"};
     return is;
 }
